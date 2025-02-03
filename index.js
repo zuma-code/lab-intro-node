1d43727f2ed06aa089b8631bf66f1ceef26e099b
Juliasudoku <zumar53@protonmail.com>
2025-02-03 12:50:25 +0000

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; // an array to hold the numbers in sorted order
+    this.length = 0; // keeps track of the number of items in the list
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b); // Sorts the list in ascending order
+    this.length++;
+  }
 
-  get(pos) {}
+  // Returns the item at the specified position (0-based index)
+  get(pos) {
+    
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  // Returns the maximum value in the list
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  // Returns the minimum value in the list
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  avg() {}
-}
+  // Returns the sum of all the items in the list
+  sum() {
+    return this.items.reduce((acc, item) => acc + item, 0);
+  }
+
+  // Returns the average of all the items in the list
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
+ }
+
 
 module.exports = SortedList;
